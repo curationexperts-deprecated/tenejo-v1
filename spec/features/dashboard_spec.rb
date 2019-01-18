0477432c8cc3e7707969ac085d845cb7e8c3c13a
Bess Sadler <bess@curationexperts.com>
2019-01-18 16:31:30 -0500

    Add a dashboard link to download the CSV
    Also:
      * Clean fedora between test suite runs
      * Set up factories for consistent object set up

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.feature 'Dashboard' do
+  context 'as a regular user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      login_as user
+      visit '/dashboard'
+    end
+
+    scenario 'can download a CSV template' do
+      expect(page).to have_link 'Download CSV Template'
+    end
+  end
+end
