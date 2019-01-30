576960c848b387b79313e09f2385c0b233b0d5f4
val99erie <val99erie@gmail.com>
2019-01-30 14:18:13 -0600

    When importing CSV records, add them to the specified collection.
    Story #101

--- app/importers/modular_importer.rb
+++ app/importers/modular_importer.rb
@@ -2,15 +2,16 @@
 require 'darlingtonia'
 
 class ModularImporter
-  def initialize(csv_file)
+  def initialize(csv_file, collection_id)
     @csv_file = csv_file
+    @collection_id = collection_id
   end
 
   def import
     raise "Cannot find expected input file #{@csv_file}" unless File.exist?(@csv_file)
 
     file = File.open(@csv_file)
-    Darlingtonia::Importer.new(parser: Darlingtonia::CsvParser.new(file: file), record_importer: Darlingtonia::HyraxRecordImporter.new).import
+    Darlingtonia::Importer.new(parser: Darlingtonia::CsvParser.new(file: file), record_importer: Darlingtonia::HyraxRecordImporter.new(collection_id: @collection_id)).import
     file.close
   end
 end
--- spec/importers/modular_importer_spec.rb
+++ spec/importers/modular_importer_spec.rb
@@ -6,34 +6,42 @@ require 'active_fedora/cleaner'
 RSpec.describe ModularImporter, :clean do
   let(:modular_csv) { 'spec/fixtures/csv_import/modular_input.csv' }
   let(:user) { ::User.batch_user }
+  let(:collection) { FactoryBot.create(:collection) }
 
   before do
     ENV['IMPORT_PATH'] = File.expand_path('../fixtures/images', File.dirname(__FILE__))
   end
 
   it "imports a csv" do
-    expect { ModularImporter.new(modular_csv).import }.to change { Work.count }.by 3
+    expect { ModularImporter.new(modular_csv, collection.id).import }.to change { Work.count }.by 3
   end
 
   it "puts the title into the title field" do
-    ModularImporter.new(modular_csv).import
+    ModularImporter.new(modular_csv, collection.id).import
     expect(Work.where(title: 'A Cute Dog').count).to eq 1
   end
 
   it "puts the url into the source field" do
-    ModularImporter.new(modular_csv).import
+    ModularImporter.new(modular_csv, collection.id).import
     expect(Work.where(source: 'https://www.pexels.com/photo/animal-blur-canine-close-up-551628/').count).to eq 1
   end
 
   it "creates publicly visible objects" do
-    ModularImporter.new(modular_csv).import
+    ModularImporter.new(modular_csv, collection.id).import
     imported_work = Work.first
     expect(imported_work.visibility).to eq 'open'
   end
 
   it "attaches files" do
     allow(AttachFilesToWorkJob).to receive(:perform_later)
-    ModularImporter.new(modular_csv).import
+    ModularImporter.new(modular_csv, collection.id).import
     expect(AttachFilesToWorkJob).to have_received(:perform_later).exactly(3).times
+  end
+
+  it "adds the new record to the collection" do
+    expect(Work.count).to eq 0
+    ModularImporter.new(modular_csv, collection.id).import
+    work = Work.first
+    expect(work.member_of_collections).to eq [collection]
   end
 end
