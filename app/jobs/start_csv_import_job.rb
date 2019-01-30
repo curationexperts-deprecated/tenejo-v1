00273ed1f129690ab2b9c8664419b0faaa545261
val99erie <val99erie@gmail.com>
2019-01-30 14:18:13 -0600

    When importing CSV records, add them to the specified collection.
    Story #101

--- app/jobs/start_csv_import_job.rb
+++ app/jobs/start_csv_import_job.rb
@@ -6,7 +6,7 @@ class StartCsvImportJob < ApplicationJob
   def perform(csv_import_id)
     csv_import = CsvImport.find csv_import_id
     csv_file_path = csv_import.manifest.to_s
-    importer = ModularImporter.new(csv_file_path)
+    importer = ModularImporter.new(csv_file_path, csv_import.fedora_collection_id)
     importer.import
   end
 end
