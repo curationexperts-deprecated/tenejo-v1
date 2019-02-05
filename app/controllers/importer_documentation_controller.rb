09bf122d415e16669f619b8d5268836a8a75dc17
Jamie Little <jamie@jamielittle.org>
2019-02-05 20:12:32 +0000

    Set correct headers for CSV download
    This sends the CSV file to the user with
    the correct headers so that the file is downloaded
    as a utf-8 csv file.
    
    After this is merged, you'll be able to download the
    csv file from `/importer_documentation/csv`
    
    Connected to #110

--- app/controllers/importer_documentation_controller.rb
+++ app/controllers/importer_documentation_controller.rb
@@ -2,4 +2,8 @@
 class ImporterDocumentationController < ApplicationController
   def guide
   end
+
+  def csv
+    send_file Rails.root.join('app', 'assets', 'csv', 'import_manifest.csv'), type: 'text/csv; charset=utf-8', disposition: 'attachment', filename: 'import_manifest.csv'
+  end
 end
