450a4e5b52cf83c42bfccff91e76eda2e964180a
Jamie Little <jamie@jamielittle.org>
2019-02-07 17:07:18 +0000

    Revert "Merge pull request #151 from curationexperts/add-clamav"
    This reverts commit e403f143c105b3079794beeedf973bfdb57491d5, reversing
    changes made to beeeb9df10bb8768d96abb1d8940d844f8fe7a3f.

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,13 +1,2 @@
 # frozen_string_literal: true
-Hydra::Works.default_system_virus_scanner = Tenejo::VirusScanner
-
-if Rails.env.production?
-  Clamby.configure(
-    check:  false, # only used for development environment
-    daemonize: true,
-    error_clamscan_missing: true,
-    error_file_missing: true,
-    error_file_virus: true,
-    fdpass: true
-  )
-end
+ClamAV.instance.loaddb if defined? ClamAV
