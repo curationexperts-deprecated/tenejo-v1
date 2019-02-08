6be2b06e48f4df0763b5127446d48b0190a27c26
Bess Sadler <bess@curationexperts.com>
2019-02-08 10:48:51 -0500

    Thoroughly test our csv import
    This should become a systems test that thoroughly tests all the fields
    we expect should be valid in a CSV.

--- spec/system/import_from_csv_spec.rb
+++ spec/system/import_from_csv_spec.rb
@@ -2,8 +2,8 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe 'Importing records from a CSV file', type: :system, js: true do
-  let(:csv_file) { File.join(fixture_path, 'csv_import', 'csv_files_with_problems', 'extra - headers.csv') }
+RSpec.describe 'Importing records from a CSV file', :perform_jobs, type: :system, js: true do
+  let(:csv_file) { File.join(fixture_path, 'csv_import', 'good', 'all_fields.csv') }
 
   context 'logged in as an admin user' do
     let(:collection) { FactoryBot.build(:collection, title: ['Testing Collection']) }
@@ -22,16 +22,13 @@ RSpec.describe 'Importing records from a CSV file', type: :system, js: true do
 
       # Fill in and submit the form
       attach_file('csv_import[manifest]', csv_file, make_visible: true)
-      expect(page).to have_content 'extra_-_headers.csv'
+      expect(page).to have_content 'all_fields.csv'
       click_on 'Preview Import'
-
-      # We expect to see warnings for this CSV file.
-      expect(page).to have_content 'The field name "another_header_2" is not supported'
 
       # We expect to see the title of the collection on the page
       expect(page).to have_content 'Testing Collection'
 
-      expect(page).to have_content 'This import will add 3 new records.'
+      expect(page).to have_content 'This import will add 1 new records.'
 
       # There is a link so the user can cancel.
       expect(page).to have_link 'Cancel', href: new_csv_import_path(locale: I18n.locale)
@@ -41,15 +38,18 @@ RSpec.describe 'Importing records from a CSV file', type: :system, js: true do
       click_on 'Start Import'
 
       # The show page for the CsvImport
-      expect(page).to have_content 'extra_-_headers.csv'
+      expect(page).to have_content 'all_fields.csv'
       expect(page).to have_content 'Start time'
 
       # We expect to see the title of the collection on the page
       expect(page).to have_content 'Testing Collection'
 
-      # TODO: Check that the import got kicked off.
-      # e.g. Check that a job got queued.
-      # Or, let the background jobs run, and check that the expected number of records got created.
+      # Let the background jobs run, and check that the expected number of records got created.
+      expect(Work.count).to eq 1
+
+      # Ensure that all the fields got assigned as expected
+      work = Work.where(title: "*haberdashery*").first
+      expect(work.title.first).to match(/haberdashery/)
     end
   end
 end
