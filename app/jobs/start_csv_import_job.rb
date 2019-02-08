f24b09ca1e6219aca9807883a1b23cac5d370925
Jamie Little <jamie@jamielittle.org>
2019-02-08 18:01:36 +0000

    Add logging
    This adds a `LogStream` class which is used by
    darlingtonia to log imports. The log files
    are located in log and are named differently
    depending on what environment you are in.
    
    The log file contains the record being imported,
    the user depositing the record, the collection id,
    and the batch ID. (These are all timestamped).
    
    Connceted to #143

--- app/jobs/start_csv_import_job.rb
+++ app/jobs/start_csv_import_job.rb
@@ -5,7 +5,9 @@ class StartCsvImportJob < ApplicationJob
 
   def perform(csv_import_id)
     csv_import = CsvImport.find csv_import_id
-    importer = ModularImporter.new(csv_import)
+    log_stream = Tenejo::LogStream.new
+    log_stream << "Starting import with batch ID: #{csv_import_id}"
+    importer = ModularImporter.new(csv_import, log_stream: log_stream)
     importer.import
   end
 end
