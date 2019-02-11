9a560b5cfe80f60449697c034092390bc644b56b
Jamie Little <jamie@jamielittle.org>
2019-02-11 20:31:15 +0000

    Use the Darlingtonia default logger
    This changes the importer code to use the
    default darlingtonia importer.
    
    Connected to #167

--- app/importers/modular_importer.rb
+++ app/importers/modular_importer.rb
@@ -2,12 +2,11 @@
 require 'darlingtonia'
 
 class ModularImporter
-  def initialize(csv_import, log_stream:)
+  def initialize(csv_import)
     @csv_file = csv_import.manifest.to_s
     @collection_id = csv_import.fedora_collection_id
     @user_id = csv_import.user_id
     @user_email = User.find(csv_import.user_id).email
-    @log_stream = log_stream
   end
 
   def import
@@ -20,12 +19,7 @@ class ModularImporter
 
     file = File.open(@csv_file)
 
-    Darlingtonia.config do |config|
-      config.default_error_stream = @log_stream
-      config.default_info_stream = @log_stream
-    end
-
-    @log_stream << "Import for collection: #{@collection_id} started by #{@user_email}"
+    Darlingtonia.config.default_info_stream << "Import for collection: #{@collection_id} started by #{@user_email}\n"
 
     Darlingtonia::Importer.new(parser: Darlingtonia::CsvParser.new(file: file), record_importer: Darlingtonia::HyraxRecordImporter.new(attributes: attrs)).import
 
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Tenjeo
   class Application < Rails::Application
+    require 'darlingtonia'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
