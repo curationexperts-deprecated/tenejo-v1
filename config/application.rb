48cb3b79f426e61dd9fd3be4bfa74a9ad46cfab0
Jamie Little <jamie@jamielittle.org>
2019-07-09 12:44:36 -0500

    Ignore RecordNotFound exception
    Right now when you try to access a record
    that does not exist it triggers a RecordNotFound exception
    and returns a 500 instead of 404. This adds some
    Rack middleware that will catch that exception and
    return a 404 instead.
    
    The code is also returning a 500 if you try
    to go to a patch that does not exist. This adds
    a route that will send you to a 404 page if you
    go to a non-existant route.
    
    Related to tenejo#227

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 require_relative 'boot'
+require_relative 'exception_middleware'
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-module Tenjeo
+module Tenejo
   class Application < Rails::Application
     require 'darlingtonia'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     config.active_job.queue_adapter = :sidekiq
+    config.middleware.use(::ExceptionMiddleware)
   end
 end
