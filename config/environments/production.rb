e4257602c5a863c976e8266e4c914c034d261234
Jamie Little <jamie@jamielittle.org>
2019-07-10 11:40:47 -0500

    Add missing space

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tenjeo_#{Rails.env}"
-  #config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
