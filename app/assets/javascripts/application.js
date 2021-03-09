8ce8a82eab53c223b3edd9c0d8ecaf8c43353539
McClain Looney <m@loonsoft.com>
2021-03-09 17:24:06 -0600

    Dynamically update copyright year

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 // Required by Blacklight
 //= require jquery
 //= require jquery_ujs
+//= require browse_everything
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require blacklight/blacklight
