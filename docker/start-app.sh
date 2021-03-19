8c5d6cab2abe31d789d97ff0d65c818b14f19f87
Max Kadel <max@curationexperts.com>
2021-03-19 12:43:08 -0400

    Add setup commands to docker startup (#324)
    * Add setup commands to docker startup
    
    - Pull in change to database config so tests still pass locally & in ci (separate DBs)
    
    * db config - c'mon little orb! find the database!
    
    * add guard around standard user creation 
    
    - so that it is not accidentally run in production, individual user seed methods can still be run in production

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -3,4 +3,7 @@
 bundle check || bundle install
 find . -name *.pid -delete
 bundle exec rake db:create && bundle exec rake db:migrate
+bundle exec rake tenejo:standard_users_setup
+bundle exec rails hyrax:default_collection_types:create
+bundle exec rails hyrax:default_admin_set:create
 bundle exec rails s -b 0.0.0.0
