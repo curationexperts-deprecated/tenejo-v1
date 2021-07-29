68017bd9b72dcd6fc17b717505c05af829ae300e
Max Kadel <max@curationexperts.com>
2021-07-29 10:32:52 -0400

    Add invitable schema and button to admin page

--- spec/system/manage_users_spec.rb
+++ spec/system/manage_users_spec.rb
@@ -10,9 +10,19 @@ RSpec.describe 'Manage users', type: :system do
       login_as admin
       visit '/admin/users'
     end
-    xit "has a button to add a user" do
+    it "has a button to add a user" do
       expect(page).to have_content("Manage Users")
       expect(page).to have_button("Invite New User")
+    end
+  end
+
+  context "as a regular user" do
+    let(:user) { FactoryBot.create(:user) }
+    before do
+      login_as user
+    end
+    it "cannot see the 'Send Invitation' page" do
+      true
     end
   end
 end
