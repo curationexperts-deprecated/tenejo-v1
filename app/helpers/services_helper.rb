861fe693d607b71876de6c6556c7ffb4e774ac97
Max Kadel <max@curationexperts.com>
2021-08-18 12:12:40 -0400

    Refactor services warnings into ServicesHelper (#365)

---
+++ app/helpers/services_helper.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module ServicesHelper
+  def services
+    ["antivirus", "image conversion", "media processing"]
+  end
+
+  def service_running?(service)
+    case service
+    when "antivirus"
+      check_antivirus_service
+    when "image conversion"
+      check_image_conversion
+    when "media processing"
+      check_audiovisual_conversion
+    end
+  end
+
+  def check_antivirus_service
+    image_path = Rails.root.join("spec", "fixtures", "images", "birds.jpg")
+    begin
+      Clamby.safe?(image_path.to_s)
+    rescue => error
+      Rails.logger.error "There was a problem when testing for Clamby / ClamAV: #{error.message} \n"
+      false
+    end
+  end
+
+  def check_image_conversion
+    image_path = Rails.root.join("spec", "fixtures", "images", "birds.jpg")
+    begin
+      MiniMagick::Image.open(image_path).validate!
+      true
+    rescue => error
+      Rails.logger.error "There was a problem when testing for MiniMagick: #{error.message} \n"
+      false
+    end
+  end
+
+  def check_audiovisual_conversion
+    Open3.capture3('ffmpeg -codecs')
+    true
+  rescue StandardError
+    Rails.logger.error('Unable to find ffmpeg')
+    false
+  end
+end
