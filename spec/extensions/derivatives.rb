aea9ed6de4d7487dc88eb5ad8c8d85d1112a45aa
McClain Looney <mcclain@curationexperts.com>
2021-08-19 10:23:01 -0500

    pdtd v nmng fr clrty

--- spec/extensions/derivatives.rb
+++ spec/extensions/derivatives.rb
@@ -10,23 +10,23 @@ RSpec.describe TenejoExtensions::PtiffDerivative, :clean do
     Hyrax::FileSetDerivativesService.new(fs)
   }
   it "creates 2 outputs" do
-    r = fsds.send(:create_image_derivatives, "./spec/fixtures/images/small.jpg")
-    expect(r.size).to eq(2)
-    expect(r.first[:url]).to match(/\/.*-ptiff.tif/)
-    expect(r.first[:label]).to match(/ptiff/)
-    expect(r.first[:format]).to match(/ptif/)
+    result = fsds.send(:create_image_derivatives, "./spec/fixtures/images/small.jpg")
+    expect(result.size).to eq(2)
+    expect(result.first[:url]).to match(/\/.*-ptiff.tif/)
+    expect(result.first[:label]).to match(/ptiff/)
+    expect(result.first[:format]).to match(/ptif/)
   end
 
   it "actually creates files" do
-    r = fsds.send(:create_image_derivatives, "./spec/fixtures/images/small.jpg")
-    u = r[0][:url].gsub("file://.", "")
-    expect(File.exist?(u)).to be true
+    result = fsds.send(:create_image_derivatives, "./spec/fixtures/images/small.jpg")
+    path = result[0][:url].gsub("file://.", "")
+    expect(File.exist?(path)).to be true
   end
 
   it "can find ptiff derivatives for fileset references" do
     fs = instance_double("fileset")
     allow(fs).to receive(:id).and_return("abc")
-    k = Hyrax::DerivativePath.new(fs, "ptiff")
-    expect(k.send(:extension)).to match '.tiff'
+    dpath = Hyrax::DerivativePath.new(fs, "ptiff")
+    expect(dpath.send(:extension)).to match '.tiff'
   end
 end
