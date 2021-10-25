cfeab8554d0be31d0f49a978de1b2458103f1028
McClain Looney <mcclain@curationexperts.com>
2021-10-25 12:56:23 -0500

    Adds more preflight features & graph connection

--- app/lib/tenejo/preflight.rb
+++ app/lib/tenejo/preflight.rb
@@ -4,11 +4,12 @@ require 'active_model'
 module Tenejo
   class PreFlightObj
     include ActiveModel::Validations
-    attr_accessor :lineno
+    attr_accessor :lineno, :children
     def initialize(h, lineno)
       self.lineno = lineno
       h.delete(:object_type)
       h.each { |k, v| send("#{k}=", v) if v.present? }
+      @children = []
     end
   end
 
@@ -17,42 +18,81 @@ module Tenejo
     validates_each :parent, :file do |rec, attr, val|
       rec.errors.add attr, "is required" if val.blank?
     end
+
     def initialize(h, lineno)
       f = h.delete(:files)
-      h[:file] = f
+      h[:file] = f.last if f
       super h, lineno
     end
   end
 
   class PFWork < PreFlightObj
-    attr_accessor :title, :identifier, :deduplication_key, :creator, :keyword,
+    attr_accessor :title, :identifier, :deduplication_key, :creator, :keyword, :files,
       :visibility, :license, :parent, :rights_statement, :resource_type,
       :abstract_or_summary, :date_created, :subject, :language
     validates_each :title, :identifier, :deduplication_key, :creator, :keyword, :visibility, :license, :parent do |rec, attr, val|
       rec.errors.add attr, "is required" if val.blank?
     end
+    def initialize(row, lineno)
+      @files = []
+      super
+    end
   end
 
   class PFCollection < PreFlightObj
-    attr_accessor :title, :identifier, :deduplication_key, :creator, :keyword, :visibility, :license, :parent, :resource_type, :abstract_or_summary, :contributor
+    attr_accessor :title, :identifier, :deduplication_key, :creator, :keyword,
+      :visibility, :license, :parent, :resource_type, :abstract_or_summary, :contributor
     validates_each :title, :identifier, :deduplication_key, :creator, :keyword, :visibility do |rec, attr, val|
       rec.errors.add attr, "is required" if val.blank?
     end
   end
   class Preflight
     def self.read_csv(input)
-      csv = CSV.open(input, headers: true, return_headers: true, skip_blanks: true,
-                     header_converters: [->(m) { m.downcase.tr(' ', '_').to_sym }])
-      output = Hash.new { |h, k| h[k] = [] }
-      output[:fatal_errors] = []
-      output[:warnings] = []
-      csv.each do |row|
-        next if (csv.lineno == 1) || row.to_h.values.all?(nil) # this is because csv.headers is nonsensical
-        output = parse_to_type row, csv.lineno, output
+      begin
+        csv = CSV.open(input, headers: true, return_headers: true, skip_blanks: true,
+                       header_converters: [->(m) { m.downcase.tr(' ', '_').to_sym }])
+        graph = Hash.new { |h, k| h[k] = [] }
+        graph[:fatal_errors] = []
+        graph[:warnings] = []
+        csv.each do |row|
+          next if (csv.lineno == 1) || row.to_h.values.all?(nil) # this is because csv.headers is nonsensical
+          graph = parse_to_type row, csv.lineno, graph
+        end
+        graph[:fatal_errors] << "No data was detected" if (graph[:work] + graph[:file] + graph[:collection]).empty?
+      rescue CSV::MalformedCSVError
+        graph[:fatal_errors] << "Could not recognize this file format"
+      ensure
+        csv.close
       end
-      csv.close
-      output[:fatal_errors] << "No data was detected" if (output[:work] + output[:file] + output[:collection]).empty?
-      output
+      connect_works(connect_files(graph))
+    end
+
+    def self.index(c, key: :identifier)
+      c.index_by { |v| v.send(key); }
+    end
+
+    def self.connect_works(graph)
+      idx = index(graph[:collection]).merge(index(graph[:work]))
+      (graph[:work] + graph[:collection]).each do |f|
+        if idx.key?(f.parent)
+          idx[f.parent].children << f
+        elsif f.parent.present?
+          graph[:warnings] << %/Could not find parent work "#{f.parent}" for work "#{f.identifier}" on line #{f.lineno}/
+        end
+      end
+      graph
+    end
+
+    def self.connect_files(graph)
+      idx = index(graph[:work])
+      graph[:file].each do |f|
+        if idx.key?(f.parent)
+          idx[f.parent].files << f
+        else
+          graph[:warnings] << %/Could not find parent work "#{f.parent}" for file "#{f.file}" on line #{f.lineno}/
+        end
+      end
+      graph
     end
 
     def self.parse_to_type(row, lineno, output)
--- spec/lib/tenejo/preflight_spec.rb
+++ spec/lib/tenejo/preflight_spec.rb
@@ -4,6 +4,12 @@ require 'spec_helper'
 require 'byebug'
 
 RSpec.describe Tenejo::Preflight do
+  context "a file that isn't a csv " do
+    let(:graph) { described_class.read_csv("spec/fixtures/images/cat.jpg") }
+    it "returns a fatal error" do
+      expect(graph[:fatal_errors]).to eq ["Could not recognize this file format"]
+    end
+  end
   context "a file with no data" do
     let(:graph) { described_class.read_csv("spec/fixtures/csv/empty.csv") }
     it "returns an empty graph" do
@@ -27,21 +33,43 @@ RSpec.describe Tenejo::Preflight do
   context "a well formed file" do
     let(:graph) { described_class.read_csv("spec/fixtures/csv/fancy.csv") }
     it "records line number" do
-      expect(graph[:work].first.lineno).to be 3
-      expect(graph[:collection].first.lineno).to be 2
-      expect(graph[:file].first.lineno).to be 4
+      expect(graph[:work].first.lineno).to eq 4
+      expect(graph[:collection].first.lineno).to eq 2
+      expect(graph[:file].first.lineno).to eq 5
+    end
+
+    it "connects files with parents" do
+      expect(graph[:work].first.files.map(&:file)).to eq ['MN-02 2.png', 'MN-02 3.png']
+      expect(graph[:work][1].files.map(&:file)).to eq ["MN-02 4.png"]
+    end
+    it "connects works with worrks" do
+      expect(graph[:work][1].children.map(&:identifier)).to eq ["MPC008"]
+    end
+    it "warns about disconnected  works" do
+      expect(graph[:warnings]).to include "Could not find parent work \"NONA\" for work \"MPC009\" on line 11"
+    end
+    it "connects works and collections with parents" do
+      expect(graph[:collection].size).to eq 2
+      expect(graph[:collection].first.children.map(&:identifier)).to eq ["MPC002", "MPC003"]
+      expect(graph[:collection].last.children.map(&:identifier)).to be_empty
+    end
+    it "warns when work has no parent" do
+      expect(graph[:warnings]).to include "Could not find parent work \"NONEXISTENT\" for work \"NONACOLLECTION\" on line 3"
+    end
+    it "warns files without parent in sheet" do
+      expect(graph[:warnings]).to include "Could not find parent work \"WHUT?\" for file \"MN-02 2.whut\" on line 6"
     end
 
     it "parses out object types" do
-      expect(graph[:work].size).to be 2
-      expect(graph[:collection].size).to be 1
-      expect(graph[:file].size).to be 3
+      expect(graph[:work].size).to eq 4
+      expect(graph[:collection].size).to eq 2
+      expect(graph[:file].size).to eq 4
     end
 
     it "has validation" do
       [:work, :collection, :file].each do |x|
         graph[x].each do |y|
-          expect(y.valid?).to be true
+          expect(y.valid?).to eq true
         end
       end
     end
@@ -49,14 +77,14 @@ RSpec.describe Tenejo::Preflight do
   describe Tenejo::PFFile do
     let(:rec) { described_class.new({}, 1) }
     it "is not valid when blank" do
-      expect(rec.valid?).not_to be true
+      expect(rec.valid?).not_to eq true
       expect(rec.errors.messages).to eq file: ["is required"], parent: ["is required"]
     end
   end
   describe Tenejo::PFWork do
     let(:rec) { described_class.new({}, 1) }
     it "is not valid when blank" do
-      expect(rec.valid?).not_to be true
+      expect(rec.valid?).not_to eq true
       expect(rec.errors.messages).to eq creator: ["is required"],
         deduplication_key: ["is required"], identifier: ["is required"],
         keyword: ["is required"], license: ["is required"],
@@ -66,7 +94,7 @@ RSpec.describe Tenejo::Preflight do
   describe Tenejo::PFCollection do
     let(:rec) { described_class.new({}, 1) }
     it "is not valid when blank" do
-      expect(rec.valid?).not_to be true
+      expect(rec.valid?).not_to eq true
       expect(rec.errors.messages).to eq creator: ["is required"],
         deduplication_key: ["is required"], identifier: ["is required"],
         keyword: ["is required"], title: ["is required"], visibility: ["is required"]
